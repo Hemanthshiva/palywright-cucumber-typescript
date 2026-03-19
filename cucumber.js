7507d0e142abadcc10712e3aa8571e1b60817cd9
Hemanth Shivashankarappa <hshivashankarappa@gmail.com>
2026-03-19 16:15:12 +0000

    Updated cucumber profile to run the feature files

--- cucumber.js
+++ cucumber.js
@@ -7,6 +7,11 @@ module.exports = {
     publishQuiet: true,
   },
   ci: {
+    requireModule: ['ts-node/register'],
+    require: ['src/support/world.ts', 'src/support/hooks.ts', 'src/step-definitions/**/*.steps.ts'],
+    format: ['json:reports/json/cucumber_report.json', 'html:reports/html/cucumber_report.html'],
+    paths: ['src/features/**/*.feature'],
+    publishQuiet: true,
     parallel: 2,
     retry: 1,
   },
